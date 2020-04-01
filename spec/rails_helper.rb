67540a33d2b34b3f29f855f04dae6a5c95866878
puppe1990 <matheus.puppe90@hotmail.com>
2020-04-01 19:49:20 -0300

    frontend convert money

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,11 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+config.before(:each, type: :system) do
+  driven_by :rack_test
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
